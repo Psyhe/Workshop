6de660b5a634f745a858471fc0143a2a3101bfb8
Maria Wysoglad <93494617+Psyhe@users.noreply.github.com>
2023-02-16 21:49:14 +0100

    Delete cp2022/tests directory

--- cp2022/tests/pggp_tests/utility/WorkerId.java
+++
@@ -1,9 +0,0 @@
-package cp2022.tests.pggp_tests.utility;
-
-public class WorkerId {
-    public final int id;
-
-    public WorkerId (int id) {
-        this.id = id;
-    }
-}
--- cp2022/tests/pggp_tests/utility/workshop_actions/Action.java
+++
@@ -1,8 +0,0 @@
-package cp2022.tests.pggp_tests.utility.workshop_actions;
-
-import cp2022.tests.pggp_tests.utility.Worker;
-import cp2022.tests.pggp_tests.utility.SimulationWithBugCheck;
-
-public interface Action {
-    void doWork(SimulationWithBugCheck workshop, Worker worker, int verbose);
-}
